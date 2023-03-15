dabc64c6d62a6d8e69b209e541f88d0a440ebfcc
Md. Shehab Chowdhury <shehab15-11396@diu.edu.bd>
2023-03-15 07:45:36 +0600

    third page done

---
+++ src/Components/Third/Third.js
@@ -0,0 +1,92 @@
+import { Box, Grid, Typography } from "@mui/material";
+import React from "react";
+import img from "../../Assets/signature.png";
+import img1 from "../../Assets/welcome-img-1.jpg";
+
+const Third = () => {
+  return (
+    <Box mx={{ xs: 5, md: 10 }} my={{ xs: 10, md: 20 }}>
+      <Grid container spacing={2} sx={{ alignItems: "center" }}>
+        <Grid item xs={12} md={6} sx={{ textAlign: "start", pt: 5 }}>
+          <Typography
+            sx={{
+              fontFamily: "jest",
+              fontSize: "25px",
+              fontWeight: "bold",
+              my: "",
+              letterSpacing: "3px",
+              opacity: 0.8,
+            }}
+          >
+            Grandmaster Talk
+          </Typography>
+          <Typography
+            sx={{
+              fontFamily: "jest",
+              fontSize: "30px",
+              fontWeight: "bold",
+              mt: 1,
+              mb: 4,
+              letterSpacing: "1px",
+            }}
+          >
+            EDUCATION IS THE MOST<br></br> POWERFUL WEAPON
+          </Typography>
+          <Typography
+            sx={{
+              fontFamily: "jest",
+              fontSize: "16px",
+
+              opacity: 0.5,
+              width: "90%",
+            }}
+          >
+            Our students are talented, hard-working and full of good ideas. We
+            encourage and empower them to bring their ideas to life. Hands-on
+            opportunities are what we're all about. Our students are busy doing
+            things that matter. Take a look at the opportunities you'll have at
+            the University. The task of the modern educator is not to cut down
+            jungles, but to irrigate deserts.
+          </Typography>
+          <Typography
+            sx={{
+              mt: 3,
+              mb: 1,
+            }}
+          >
+            <img src={img} alt="Signature"></img>
+          </Typography>
+          <Typography
+            sx={{
+              fontFamily: "jest",
+              fontSize: "20px",
+              fontWeight: "bold",
+              letterSpacing: "3px",
+
+              opacity: 0.7,
+            }}
+          >
+            Katherine Gonzalez
+          </Typography>
+          <Typography
+            sx={{
+              fontFamily: "jest",
+              fontSize: "16px",
+              fontWeight: "bold",
+              letterSpacing: "3px",
+
+              opacity: 0.4,
+            }}
+          >
+            Sekolah Grandmaster
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <img src={img1} alt="collage"></img>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Third;
--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -3,12 +3,14 @@ import React from "react";
 import { Outlet } from "react-router";
 import Header from "../Components/Header/Header";
 import Second from "../Components/Second/Second";
+import Third from "../Components/Third/Third";
 
 const Main = () => {
   return (
     <Box maxWidth={"1440px"}>
       <Header></Header>
       <Second></Second>
+      <Third></Third>
       <Outlet></Outlet>
     </Box>
   );
